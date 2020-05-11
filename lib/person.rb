c43e146b5eb9d307fdcbf4cce5db7387f839454a
Jesse Garcilazo <jjgarcilazo@gmail.com>
2020-05-11 18:45:11 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,21 +1,12 @@
-class Dog
-  def initialize(name, breed="mut")
+class Person
+  def initialize(name)
     @name = name
-    @breed = breed
   end
-
-
-def name=(name)
-  @name = name
+  def name=(name)
+    @name = name
+  end
+  def name
+    @name
+  end
 end
 
-def name
- @name
-end
-def breed=(breed)
-  @breed = bread
-end
-def breed
-  @breed
-end
-end
